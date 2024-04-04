bfd70ea9e493b987fce1c4141c4ab9ef7f385dbc
Nagma Bano <bano.nagma.1@gmail.com>
2024-04-04 12:50:49 +0530

    setting up the top app bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,11 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.ui:ui:1.6.4")
+    implementation("androidx.compose.material:material:1.6.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.4")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
